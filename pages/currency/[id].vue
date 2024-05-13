545c23dc7237689f1f7c5d306d6ba69ce34f25c0
jxxl998 <jessielee1998_sc@foxmail.com>
2024-05-14 00:58:02 +0800

    feat: complete detail page

--- pages/currency/[id].vue
+++ pages/currency/[id].vue
@@ -1,3 +1,27 @@
-<script setup></script>
+<script setup>
+const route = useRoute();
 
-<template></template>
+const { data } = await useFetch("/api/tickers/?id=" + route.params.id);
+
+const coin = data.value.data[0];
+</script>
+
+<template>
+  <div>
+    <h2>{{ coin.name }} Detail page</h2>
+    <table border="1 px solid">
+      <thead>
+        <th>Symbol</th>
+        <th>Rank</th>
+        <th>Price - US $</th>
+        <th>Market Cap - US $</th>
+      </thead>
+      <tr>
+        <td>{{ coin.symbol }}</td>
+        <td>{{ coin.rank }}</td>
+        <td>{{ coin.price_usd }}</td>
+        <td>{{ coin.market_cap_usd }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
